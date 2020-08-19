49232f4ac772b1437c8234094e9cef3b14dec7b3
Zain <zainulabideenbawa@gmail.com>
2020-08-19 19:44:38 +0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use actix_web::{get, post};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Deserialize;
+
+#[get("/HandleOn/{roomID}/{ButtonID}")]
+async fn HandleOn(v: web::Path<(u32, u32)>) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "Got RoomID {} and Button ID {} to turn On",
+        v.0, v.1
+    ))
+}
+
+#[get("/HandleOff{roomID}/{ButtonID}")]
+async fn HandleOff(v: web::Path<(u32, u32)>) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "Got RoomID {} and Button ID {} to turn Off",
+        v.0, v.1
+    ))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(HandleOn).service(HandleOff))
+        .bind("127.0.0.1:5000")?
+        .run()
+        .await
+}
